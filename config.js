95cf039936b2d7f513ef917eaf91b363dd5365fd
sanz223 <azrilsanz8@gmail.com>
2026-01-08 02:57:00 +0700

    ddos-web

---
+++ config.js
@@ -0,0 +1,27 @@
+{
+    "version": "11.0",
+    "name": "CYBER DDoS Panel",
+    "author": "indonet",
+    "default_target": "",
+    "default_port": 80,
+    "default_threads": 100,
+    "default_duration": 60,
+    "attack_methods": [
+        "HTTP Flood",
+        "TCP SYN Flood", 
+        "UDP Flood",
+        "Slowloris",
+        "Mixed Attack"
+    ],
+    "user_agents": [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15",
+        "Mozilla/5.0 (Linux; Android 10; SM-G973F) AppleWebKit/537.36"
+    ],
+    "proxies": [],
+    "auto_save": true,
+    "log_attacks": true,
+    "encryption": false,
+    "max_threads": 1000,
+    "max_duration": 3600
+}
